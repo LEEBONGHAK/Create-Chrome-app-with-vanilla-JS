a20f1b30270f6a18cb6ab7e86c927df4ec628984
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-06 02:15:38 +0900

    Making a JS clock part one
    현재 시간을 얻는 코드 작성(실시간 업데이트 x)

---
+++ clock.js
@@ -0,0 +1,17 @@
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = clockContainer.querySelector("h1");
+
+// 현재 시간 불러오기
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+function init() {
+    getTime();
+}
+
+init();
