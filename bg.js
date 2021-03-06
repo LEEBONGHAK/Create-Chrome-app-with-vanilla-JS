1997e9e9294ee9d030deaaa3fbbea24dce304dd3
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-07 02:53:15 +0900

    Image Background
    새로 고침할 때마다 랜덤한 이미지를 배경화면으로 나타내는 기능 구현

---
+++ bg.js
@@ -0,0 +1,33 @@
+const body = document.querySelector('body');
+
+const IMG_NUMBER = 7;
+
+
+function handleImgLoad(image) {
+    
+}
+
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber}.jpg`;
+    image.classList.add("bgImg");
+    body.prepend(image);
+}
+
+
+// Math.random() : 0 ~ 1 까지의 랜덤한 숫자를 보여줌
+// Math.random() * 5 : 0 ~ 5 까지의 랜덤한 숫자를 보여줌
+// Math.floor() : 소수점 버림
+// Math.ceil() : 소수점 올림
+function genRandomNum() {
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init() {
+    const randomNumber = genRandomNum();
+    paintImage(randomNumber);
+}
+
+init();
