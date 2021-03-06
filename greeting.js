88f2da7980cc06e7726d317aa6ef974ecf5aee16
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-06 18:35:15 +0900

    Saving the User Name part Two
    유저 이름을 받아 표기하고 local storage에 저장하는 것을 구현

--- greeting.js
+++ greeting.js
@@ -5,8 +5,24 @@ const form = document.querySelector('.js-form'),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
-    greeting.classList.remove(SHOWING_CN);
+    form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
@@ -15,7 +31,7 @@ function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
         // 유저 없음
-
+        askForName();
     } else {
         // 유저 존재
         paintGreeting(currentUser);
