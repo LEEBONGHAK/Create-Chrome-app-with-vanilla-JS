491db968451b00419b80587bbbc2bbe044afb3e9
이봉학 <dlqhdgkr456@gmail.com>
2021-08-23 21:45:53 +0900

    Added explanations in clock.js

--- clock.js
+++ clock.js
@@ -3,7 +3,7 @@ const clockContainer = document.querySelector(".js-clock"),
 
 // 현재 시간 불러오기
 function getTime() {
-    const date = new Date();
+    const date = new Date();    // 현재 시간을 가져오는 함수로 시간을 가져와 변수에 저장
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
@@ -12,7 +12,7 @@ function getTime() {
 
 function init() {
     getTime();
-    setInterval(getTime, 1000);
+    setInterval(getTime, 1000);     // 시간 표시 간격 지정하여 반복 (ms)
 }
 
 init();
