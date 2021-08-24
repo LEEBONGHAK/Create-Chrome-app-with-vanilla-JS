e68821e7abba84849a851fc58781d73999da544d
이봉학 <dlqhdgkr456@gmail.com>
2021-08-24 14:39:50 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -3,10 +3,10 @@ const body = document.querySelector('body');
 const IMG_NUMBER = 7;
 
 function paintImage(imgNumber) {
-    const image = new Image();
+    const image = new Image();      // img tag 생성
     image.src = `images/${imgNumber}.jpg`;
     image.classList.add("bgImg");
-    body.prepend(image);
+    body.prepend(image);    // prepend() : 생성한 html tag를 가장 처음에 추가
 }
 
 
